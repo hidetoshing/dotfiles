b79e6725aeb4272b1fde5a2bbf824c02c92ea7bc
hidetoshing <hidetoshing@gmail.com>
2018-08-24 17:39:42 +0900

    :art: improve boot.sh

--- boot.sh
+++ boot.sh
@@ -2,18 +2,19 @@
 
 echo "Installer bootstrap"
 
-echo "Create ssh key"
+# ssh key create
+if [ ! -e ~/.ssh/id_rsa ]; then
+    echo "Create ssh key"
+    ssh-keygen -t rsa -b 4096
+fi
 
-ssh-keygen -t rsa -b 4096
-
-echo "install homebrew"
-
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
+# homebrew for mac
+if ! which brew > /dev/null 3>&1; then
+    echo "install homebrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 git clone git@github.com:hidetoshing/dotfiles.git ~/src/github.com/hidetoshing/dotfiles
-
-cd ~/src/github.com/hidetoshing/dotfiles
 
 echo "This dotfiles require vim, zsh, tmux."
 echo "  make install"
